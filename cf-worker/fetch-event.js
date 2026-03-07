3d9e948fa3ec49b65d0b8a175dfa409c65fb18f0
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-03-07 17:21:20 -0600

    Update fetch-event.js

--- cf-worker/fetch-event.js
+++ cf-worker/fetch-event.js
@@ -1,7 +1,10 @@
 export default {
   async fetch(request){
     const response = await fetch(`https://script.google.com/macros/s/AKfycbxNUKrGHDrD0h7korRgX-XB4qzHcOcqMHmP1i8Qd3QrgTRdRRkICEYjUQESgn_03WCNKw/exec?${Date.now()}`);
-    const text = await response.text();
+    let text = await response.text();
+    if(request.url.endsWith('jsonp')){
+      text = `export const payload = \`${text.replaceAll('`',`'`)}\``;
+    }
     return new Response(text,{headers:{"Access-Control-Allow-Origin":"*"}});
   }
 }
