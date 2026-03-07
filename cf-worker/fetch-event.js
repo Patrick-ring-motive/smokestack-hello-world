7e29d7a5a01e3a2e1e73e7aadd03f091417b4ca6
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-03-07 17:55:06 -0600

    Update fetch-event.js

--- cf-worker/fetch-event.js
+++ cf-worker/fetch-event.js
@@ -1,7 +1,12 @@
 export default {
   async fetch(request){
-    const response = await fetch(`https://script.google.com/macros/s/AKfycbxNUKrGHDrD0h7korRgX-XB4qzHcOcqMHmP1i8Qd3QrgTRdRRkICEYjUQESgn_03WCNKw/exec?${Date.now()}`);
-    let text = await response.text();
+    let text;
+    try{
+      const response = await fetch(`https://script.google.com/macros/s/AKfycbxNUKrGHDrD0h7korRgX-XB4qzHcOcqMHmP1i8Qd3QrgTRdRRkICEYjUQESgn_03WCNKw/exec?${Date.now()}`);
+      text = await response.text();
+    }catch(e){
+      text = String(e);
+    }
     if(request.url.endsWith('jsonp')){
       text = 'export const payload = `'+text.replaceAll('`',`'`)+'`';
     }
